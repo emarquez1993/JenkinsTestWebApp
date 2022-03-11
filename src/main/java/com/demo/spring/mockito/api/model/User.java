995e1c19d8bac936056bc1fb1af6c8befbd12c43
emarquez1993 <53238046+emarquez1993@users.noreply.github.com>
2022-03-11 11:03:25 -0600

    first commit

---
+++ src/main/java/com/demo/spring/mockito/api/model/User.java
@@ -0,0 +1,67 @@
+package com.demo.spring.mockito.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;*/
+
+//@Document(collection = "users")
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
+@Entity
+public class User {
+	@Id
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int id, String name, int age, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+	
+
+}
